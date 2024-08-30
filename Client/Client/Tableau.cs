82e077ebacd066110fc537408e6f2dfbd992d326
Alexandre Siaudeau <2434546@etu.cchic.ca>
2024-08-30 10:39:35 -0400

    choix bateau

--- Client/Client/Tableau.cs
+++ Client/Client/Tableau.cs
@@ -37,9 +37,41 @@ namespace Client
             return "";
         }
 
-        public string ChoixBateau()
+        public bool ChoixBateau(bool joueur)
         {
-            return "";
+            Console.WriteLine("Entrez les coordonnées de votre bateau (ex: 4,5) : ");
+            string input = Console.ReadLine();
+
+            string[] coord = input.Split(",");
+
+            if (!int.TryParse(coord[0], out int case1) || !int.TryParse(coord[1], out int case2))
+            {
+                Console.WriteLine("IL FAUT UN INT");
+                return false;
+            }
+
+            if (Math.Abs(case1 - case2) == 1)
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Vous ne pouvez placer votre bateau que de façon vertical ou horizontal");
+                return false;
+            }
+
+            string[] tableau = joueur ? tableauJoueur : tableauAdversaire;
+
+            if (tableau[case1] == "" && tableau[case2] == "")
+            {
+                tableau[case1] = "B";
+                tableau[case2] = "B";
+
+                Console.WriteLine($"Le bateau a été placé aux coordonnées {case1} et {case2}");
+                return true;
+            }
+
+            return true;
         }
 
         public string EnvoieConfirmation()
